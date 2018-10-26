0496a87a4f875a02ef305f2f0ede70b9a4789973
Imran Yousuf <iyousuf@ebay.com>
2018-10-26 14:59:43 -0700

    Hello works

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import logo from './logo.svg';
+import { HomePage } from './_screens/HomePage';
 
 import './App.css';
 
@@ -27,14 +27,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">{this.state.response}</p>
+        <HomePage />
       </div>
     );
   }
+
+
 }
 
 export default App;
---
+++ client/src/_screens/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Card, CardTitle } from '@material-ui/core/Card';
+import '../static/HomePage.css';
+
+export class HomePage extends Component {
+    render(){
+        return(
+            <div>
+                <p>Hello</p>
+            </div>
+        )
+    }
+}
